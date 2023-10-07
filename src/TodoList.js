a5e15910ef32d7d4cdace17c96047010ce246864
wjdskagjs1 <wjdskagjs1@naver.com>
2023-10-08 00:22:11 +0900

    버그 수정 및 다크모드 구현

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,47 +1,41 @@
-import React, { useEffect, useState, useRef, useLayoutEffect } from 'react';
-import dragula from 'dragula';
+import React, { useLayoutEffect, useState, useRef } from 'react';
+import dragula from 'react-dragula';
 
 const TodoList = () => {
   const [items, setItems] = useState(() => {
     const savedItems = localStorage.getItem('todoItems');
     return savedItems ? JSON.parse(savedItems) : [
-      { text: 'Task 1', checked: false },
-      { text: 'Task 2', checked: false },
-      { text: 'Task 3', checked: false }
+      { id: 1, text: 'Task 1', checked: false },
+      { id: 2, text: 'Task 2', checked: false },
+      { id: 3, text: 'Task 3', checked: false }
     ];
   });
-
-  const [newTask, setNewTask] = useState('');
-  const [editedTask, setEditedTask] = useState(null);
-  const [updatedTask, setUpdatedTask] = useState('');
-  const editInputRef = useRef(null);  // Reference for the edit input
-
-  const [isEditing, setIsEditing] = useState(false); // Flag to track if in edit mode
-
-  useEffect(() => {
-    const containers = document.querySelectorAll('.task');
-    const drake = dragula(Array.from(containers), {
-      moves: (el, source, handle, sibling) => {
-        // Disable drag during editing
-        return !isEditing;
-      }
-    });
-
-    drake.on('drop', (el, target, source, sibling) => {
-
-    });
-
-    return () => {
-      drake.destroy(); // Cleanup the dragula instance
-    };
-  }, [items, isEditing]);
 
   useLayoutEffect(()=> {
     if (editInputRef.current !== null) editInputRef.current.focus();
   })
 
+  const [newTask, setNewTask] = useState('');
+  const [editedTask, setEditedTask] = useState(null);
+  const [updatedTask, setUpdatedTask] = useState('');
+  const editInputRef = useRef(null);
+
+  const dragulaDecorator = (componentBackingInstance) => {
+    if (componentBackingInstance) {
+      let options = {};
+      const drake = dragula([componentBackingInstance], options);
+      drake.on('drop', (el, target, source, sibling) => {
+        // Handle drop here
+      });
+    }
+  };
+
   const saveItemsToLocalStorage = (items) => {
     localStorage.setItem('todoItems', JSON.stringify(items));
+  };
+
+  const generateUniqueId = () => {
+    return Math.random().toString(36).substr(2, 9);
   };
 
   const handleNewTaskChange = (event) => {
@@ -50,38 +44,40 @@ const TodoList = () => {
 
   const handleAddTask = () => {
     if (newTask.trim() !== '') {
-      const updatedItems = [...items, { text: newTask, checked: false }];
+      const updatedItems = [...items, { id: generateUniqueId(), text: newTask, checked: false }];
       setItems(updatedItems);
       saveItemsToLocalStorage(updatedItems);
       setNewTask('');
     }
-  };  
+  };
 
-  const handleEditTask = (index) => {
-    setUpdatedTask(items[index].text);  // Store the task text for editing
-    setEditedTask(index);
-    setIsEditing(true); // Enable edit mode
-  };  
+  const handleEditTask = (id) => {
+    const index = items.findIndex(item => item.id === id);
+    if (index !== -1) {
+      setUpdatedTask(items[index].text);
+      setEditedTask(index);
+    }
+  };
 
-  const handleUpdateTask = (index) => {
-    const updatedItems = [...items];
-    updatedItems[index] = {
-      ...updatedItems[index],
-      text: updatedTask  // Update the task text
-    };
+  const handleUpdateTask = (id) => {
+    const updatedItems = items.map(item => {
+      if (item.id === id) {
+        return { ...item, text: updatedTask };
+      }
+      return item;
+    });
     setItems(updatedItems);
     saveItemsToLocalStorage(updatedItems);
     setEditedTask(null);
-    setIsEditing(false); // Disable edit mode
+    setUpdatedTask('');
   };
-  
 
-  const handleDeleteTask = (index) => {
-    const updatedItems = [...items];
-    updatedItems.splice(index, 1);
+  const handleDeleteTask = (id) => {
+    const updatedItems = items.filter(item => item.id !== id);
     setItems(updatedItems);
     saveItemsToLocalStorage(updatedItems);
   };
+
   const addItemEnter = key =>{
     if(key === 'Enter') {
 	  	handleAddTask();
@@ -93,71 +89,84 @@ const TodoList = () => {
 	  }
   }
 
-  const handleToggleCheckbox = (index) => {
-    const updatedItems = [...items];
-    updatedItems[index] = {
-      ...updatedItems[index],
-      checked: !updatedItems[index].checked
-    };
+  const handleToggleCheckbox = (id) => {
+    const updatedItems = items.map(item => {
+      if (item.id === id) {
+        return { ...item, checked: !item.checked};
+      }
+      return item;
+    });
     setItems(updatedItems);
     saveItemsToLocalStorage(updatedItems);
   };
 
   return (
-    <div className="wrapper">
-      <div className="task-input">
+    <div className="wrapper p-4">
+      <div className="task-input flex">
         <input
           type="text"
           placeholder='입력 후 엔터'
           value={newTask}
           onChange={handleNewTaskChange}
           onKeyDown={(e) => addItemEnter(e.key)}
+          className="flex-grow p-2 border border-gray-300 rounded"
         />
-        {/* <button onClick={handleAddTask}><img src="plus-square-icon.svg" alt="plus"/></button> */}
+        <button
+          onClick={handleAddTask}
+          className="ml-2 w-12 h-12 bg-green-500 text-black p-2 rounded"
+        >
+          <img src="plus-square-line-icon.svg" alt="plus" className="ml-1 w-6 h-6" />
+        </button>
       </div>
-      <div className="task-list">
-      {items.map((task, index) => (
-        <div key={index}
-        className="task">
-          {editedTask === index ? (
-            <div>
-              <input
-                ref={editInputRef}
-                type="text"
-                name="content"
-                value={updatedTask}
-                onChange={(e) => setUpdatedTask(e.target.value)}
-                onKeyDown={(e) => updateItemEnter(e.key, index)}
-              />
-              <button onClick={() => handleUpdateTask(index)}><img src="check-mark-box-icon.svg" alt="Done"/></button>
-            </div>
-          ) : (
-            <div>
-              <input
-                type="checkbox"
-                checked={task.checked}
-                onChange={() => handleToggleCheckbox(index)}
-              />
-              <span name="content" style={{ textDecoration: task.checked ? 'line-through' : 'none' }}>
-                {task.text}
-              </span>
-              <button
-                  className="ml-2 bg-blue-500 text-white p-2 rounded"
-                  onClick={() => handleEditTask(index)}>
-                  <img src="edit-box-icon.svg" alt="Edit" className="w-4 h-4"/>
-              </button>
-              <button
-                  className="ml-2 bg-red-500 text-white p-2 rounded"
-                  onClick={() => handleDeleteTask(index)}>
-                  <img src="trash-can-icon.svg" alt="Delete" className="w-4 h-4"/>
-              </button>
-            </div>
-          )}
-        </div>
-      ))}
+      <div className="task-list mt-4" ref={dragulaDecorator}>
+        {items.map((task, index) => (
+          <div key={task.id} className="task items-center justify-between mb-2 p-2 border rounded">
+            {editedTask === index ? (
+              <div className="flex items-center">
+                <input
+                  ref={editInputRef}
+                  type="text"
+                  name="content"
+                  value={updatedTask}
+                  onChange={(e) => setUpdatedTask(e.target.value)}
+                  onKeyDown={(e) => updateItemEnter(e.key, task.id)}
+                  className="flex-grow p-2 bg-black rounded"
+                />
+                <button onClick={() => handleUpdateTask(task.id)} className="ml-2 bg-blue-500 text-white p-2 rounded">
+                  <img src="check-mark-box-icon.svg" alt="Done" className="w-4 h-4" />
+                </button>
+              </div>
+            ) : (
+              <div className="flex">
+                <input
+                  type="checkbox"
+                  checked={task.checked}
+                  onChange={() => handleToggleCheckbox(task.id)}
+                  className="mr-2 w-8"
+                />
+                <div name="content" className={ 'w-full '+(task.checked ? 'line-through' : '')}>
+                  {task.text}
+                </div>
+                <button
+                  className="ml-2 w-8 bg-blue-500 text-white p-2 rounded"
+                  onClick={() => handleEditTask(task.id)}
+                >
+                  <img src="edit-box-icon.svg" alt="Edit" className="w-4 h-4" />
+                </button>
+                <button
+                  className="ml-2 w-8 bg-red-500 text-white p-2 rounded"
+                  onClick={() => handleDeleteTask(task.id)}
+                >
+                  <img src="trash-can-icon.svg" alt="Delete" className="w-4 h-4" />
+                </button>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
+  
 };
 
 export default TodoList;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     './public/index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      darkMode: 'class',
+    },
   },
   plugins: [],
 }
